2e7118a9112cfebb456921f5c9b81f26d93499df
mjancic <mitjajancic@gmail.com>
2021-05-29 14:54:26 +0200

    Added validation declaration.

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
-import '@babel/polyfill'
-import 'mutationobserver-shim'
-import Vue from 'vue'
-import './plugins/bootstrap-vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import '@babel/polyfill';
+import 'mutationobserver-shim';
+import Vue from 'vue';
+import './plugins/bootstrap-vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 import { makeServer } from "./server";
-import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue';
+import Vuelidate from 'vuelidate';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 if (process.env.NODE_ENV === 'development' && process.env.VUE_APP_USE_MOCK_API === 'true') {
   makeServer()
@@ -16,9 +17,10 @@ if (process.env.NODE_ENV === 'development' && process.env.VUE_APP_USE_MOCK_API =
 
 Vue.use(BootstrapVue)
 Vue.use(BootstrapVueIcons)
+Vue.use(Vuelidate)
 
 new Vue({
   router,
   store: store.original,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
