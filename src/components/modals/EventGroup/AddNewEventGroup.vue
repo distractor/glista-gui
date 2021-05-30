dc0e6293b8f0280ba58b425d963d8689bbc7db1a
mjancic <mitjajancic@gmail.com>
2021-05-30 10:49:04 +0200

    Validator messages shown. Scoring details printed.

--- src/components/modals/EventGroup/AddNewEventGroup.vue
+++ src/components/modals/EventGroup/AddNewEventGroup.vue
@@ -1,33 +1,57 @@
 <template>
   <b-modal :visible.sync="modalVisible" size="md" title="Add new event group" centered header-bg-variant="light" ok-title="Add" ok-variant="info" @hide="hideModal()">
     <b-container fluid>
-      <b-form-group>
-        <b-row>
-          <b-col>
-            <b-form-group inline>
-              <label class="mr-sm-2" for="event-group-select">Name:</label>
-              <b-form-input id="event-group-name" class="mb-2 mr-sm-2 mb-sm-0" placeholder="2021 SLO league" v-model="newEventGroup.name" :state="!$v.newEventGroup.name.$invalid"></b-form-input>
-            </b-form-group>
-          </b-col>
-        </b-row>
-      </b-form-group>
-      <div v-if="loadingScorings">
-        Please wait while data is loading ...
+      <b-form>
         <b-form-group>
-          <b-spinner type="grow" label="Spinning"></b-spinner>
+          <label class="mr-sm-2" for="event-group-select">Name:</label>
+          <b-form-input id="event-group-name" class="mb-2 mr-sm-2 mb-sm-0" placeholder="2021 SLO league" v-model="newEventGroup.name" :state="!$v.newEventGroup.name.$invalid" aria-describedby="event-group-name-feedback"></b-form-input>
+          <b-form-invalid-feedback id="event-group-name-feedback">This is a required field.</b-form-invalid-feedback>
         </b-form-group>
-      </div>
-      <div v-else>
-        <b-form-group>
-          <b-row>
-            <b-col>
-              <b-form inline>
-                <label class="mr-sm-2" for="scoring-select">Select scoring:</label>
-                <b-form-select id="scoring-selector" class="mb-2 mr-sm-2 mb-sm-0" :options="listOfScorings" v-model="newEventGroup.scoringId" text-field="name" value-field="id" v-if="!loadingScorings" :state="!$v.newEventGroup.scoringId.$invalid"></b-form-select>
-              </b-form>
-            </b-col>
-          </b-row>
-        </b-form-group>
+        <div v-if="loadingScorings">
+          Please wait while data is loading ...
+          <b-form-group>
+            <b-spinner type="grow" label="Spinning"></b-spinner>
+          </b-form-group>
+        </div>
+        <div v-else>
+          <b-form-group>
+            <label class="mr-sm-2" for="scoring-select">Select scoring:</label>
+            <b-form-select id="scoring-selector" class="mb-2 mr-sm-2 mb-sm-0" :options="listOfScorings" v-model="newEventGroup.scoringId" text-field="name" value-field="id" v-if="!loadingScorings" :state="!$v.newEventGroup.scoringId.$invalid" @change="onSelectedScoringChange" aria-describedby="event-group-scoringid-feedback"></b-form-select>
+            <b-form-invalid-feedback id="event-group-scoringid-feedback">This is a required field.</b-form-invalid-feedback>
+          </b-form-group>
+        </div>
+      </b-form>
+      <div v-if="!isNullOrEmpty(newEventGroup.scoringId)">
+        <b-card header="Selected scoring details">
+          <b-card-text>
+            <ul style="list-style-type:none;">
+              <li>
+                <strong>Name:</strong> {{selectedScoring.name}}
+              </li>
+              <li>
+                <strong>Type:</strong> {{selectedScoring.type}}
+              </li>
+              <li>
+                <strong>Includes nationalities:</strong>
+                <ul>
+                  <li v-for="nation in selectedScoring.nationalities" :key="nation">{{nation}}</li>
+                </ul>
+              </li>
+              <li>
+                <strong>Scoring tables:</strong>
+                <ul>
+                  <li v-for="table in selectedScoring.tables" :key="table">{{table}}</li>
+                </ul>
+              </li>
+              <li>
+                <strong>Points:</strong>
+                <ul>
+                  <li v-for="(point, index) in selectedScoring.points" :key="point">rank: {{index + 1}}. -> points: {{point}}</li>
+                </ul>
+              </li>
+            </ul>
+          </b-card-text>
+        </b-card>
       </div>
     </b-container>
     <template #modal-footer>
@@ -58,6 +82,7 @@ export default Vue.extend({
       addingNewEventGroup: false,
       eventGroupAdded: false,
       errorMessage: "",
+      selectedScoring: {} as ScoringDTO,
 
       // Lists.
       listOfScorings: [] as Array<ScoringDTO>,
@@ -91,9 +116,20 @@ export default Vue.extend({
 
 
   methods: {
-    vuelidateTouch(validation: any) {
-      ServiceHelper.VuelidateTouch(validation);
+    /**
+     * Is null or empty helper.
+     */
+    isNullOrEmpty(obj: any): boolean {
+      return ServiceHelper.IsNullOrEmpty(obj)
     },
+
+    /**
+     * Execute on selected scoring change.
+     */
+    onSelectedScoringChange() {
+      this.selectedScoring = this.listOfScorings.find(s => s.id == this.newEventGroup.scoringId) as ScoringDTO
+    },
+
     /**
      * Close modal and reload event groups.
      */
